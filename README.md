c373e4504fcde84e030f6c8a4793d47ddb24a5eb
juanrabaro <jrbaro@iesrafaelalberti.es>
2023-10-27 17:42:29 +0200

    subida archivos

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # A02Proyecto-Mensajes-LS_BaroLopezJuanRamon_DWEC
+
+## Ejercicio mensajes + localstorage
+
+* [Ejercicio](https://juanrabaro.github.io/A02Proyecto-Mensajes-LS_BaroLopezJuanRamon_DWEC/index.html)
---
+++ js/app.js
@@ -0,0 +1,72 @@
+const boton = document.querySelector("input")
+const contenedorLista = document.querySelector("#lista-mensajes")
+
+var listaMensajes = []
+
+function cargaLocalStorage() {
+    listaMensajes = JSON.parse(localStorage.getItem("mensajes"))
+    listaMensajes.forEach(mensaje => {
+        const li = document.createElement("li")
+        li.innerHTML = 
+        `
+            ${mensaje}
+            <a href="#" class="borrarFila" id="${listaMensajes.indexOf(mensaje)}">X</a>
+        `
+        contenedorLista.appendChild(li)
+    })
+}
+
+cargaLocalStorage()
+
+boton.addEventListener("click", () => {
+    añadirHTML()
+})
+
+function añadirHTML() {
+    const mensaje = document.querySelector("#mensaje").value
+    limpiarHTML()
+    listaMensajes.push(mensaje)
+    localStorage.setItem("mensajes", JSON.stringify(listaMensajes))
+
+    listaMensajes.forEach(mensaje => {
+        const li = document.createElement("li")
+        li.innerHTML = 
+        `
+            ${mensaje}
+            <a href="#" class="borrarFila" id="${listaMensajes.indexOf(mensaje)}">X</a>
+        `
+        contenedorLista.appendChild(li)
+    })
+
+}
+
+function limpiarHTML() {
+    while (contenedorLista.firstChild) {
+        contenedorLista.firstChild.remove()
+    }
+}
+
+function actualizarHTML() {
+    limpiarHTML()
+    
+    listaMensajes = JSON.parse(localStorage.getItem("mensajes"))
+
+    listaMensajes.forEach(mensaje => {
+        const li = document.createElement("li")
+        li.innerHTML = 
+        `
+            ${mensaje}
+            <a href="#" class="borrarFila" id="${listaMensajes.indexOf(mensaje)}">X</a>
+        `
+        contenedorLista.appendChild(li)
+    })
+}
+
+
+contenedorLista.addEventListener("click", (e) => {
+    e.preventDefault()
+    const id = e.target.getAttribute("id")
+    listaMensajes.splice(id, 1)
+    localStorage.setItem("mensajes", JSON.stringify(listaMensajes))
+    actualizarHTML()
+})
